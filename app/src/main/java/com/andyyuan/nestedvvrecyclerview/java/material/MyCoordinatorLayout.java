4c58a5a003a77729b949f7ccfd874d9859a8364d
zongjiayuan <15800308802@163.com>
2021-11-24 15:22:27 +0800

    add new scroll change.

--- app/src/main/java/com/andyyuan/nestedvvrecyclerview/java/material/MyCoordinatorLayout.java
+++ app/src/main/java/com/andyyuan/nestedvvrecyclerview/java/material/MyCoordinatorLayout.java
@@ -68,24 +68,24 @@ public class MyCoordinatorLayout extends CoordinatorLayout implements NestedScro
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-        switch (ev.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                xDistance = yDistance = 0f;
-                xLast = ev.getX();
-                yLast = ev.getY();
-                break;
-            case MotionEvent.ACTION_MOVE:
-                final float curX = ev.getX();
-                final float curY = ev.getY();
-                xDistance += Math.abs(curX - xLast);
-                yDistance += Math.abs(curY - yLast);
-                xLast = curX;
-                yLast = curY;
-                return isNeedScroll;
-            case MotionEvent.ACTION_UP:
-                break;
-
-        }
+//        switch (ev.getAction()) {
+//            case MotionEvent.ACTION_DOWN:
+//                xDistance = yDistance = 0f;
+//                xLast = ev.getX();
+//                yLast = ev.getY();
+//                break;
+//            case MotionEvent.ACTION_MOVE:
+//                final float curX = ev.getX();
+//                final float curY = ev.getY();
+//                xDistance += Math.abs(curX - xLast);
+//                yDistance += Math.abs(curY - yLast);
+//                xLast = curX;
+//                yLast = curY;
+//                return isNeedScroll;
+//            case MotionEvent.ACTION_UP:
+//                break;
+//
+//        }
         return super.onInterceptTouchEvent(ev);
     }
 
